faddf4a5cec96c058e38ed58591687eefb7c100e
sean-garwood <seansgarwood@gmail.com>
2024-12-31 12:10:04 -0500

    feat: webpack config, package.json
    * formatting
    * update year
    * update header

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # My Web Project
 
 ## Overview
+
 This project is a simple web application that demonstrates the use of HTML, CSS, and JavaScript to create an interactive webpage.
 
 ## Project Structure
+
 ```
 my-web-project
 ├── src
@@ -17,19 +19,26 @@ my-web-project
 ```
 
 ## Setup Instructions
+
 1. Clone the repository:
+
    ```
    git clone <repository-url>
    ```
+
 2. Navigate to the project directory:
+
    ```
    cd my-web-project
    ```
+
 3. Open the `src/index.html` file in your web browser to view the project.
 
 ## Usage
+
 - Modify the `src/styles/style.css` file to change the appearance of the webpage.
 - Update the `src/scripts/app.js` file to add or modify functionality.
 
 ## Contributing
+
 Feel free to submit issues or pull requests for improvements or bug fixes.
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+import { resolve, dirname } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import path from "path";
+import { type } from "os";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+    },
+  },
+};
